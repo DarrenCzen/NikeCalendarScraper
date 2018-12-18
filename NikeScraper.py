5e5a7526b1ff4a034d6f9088d3bc99bca648df2c
Darren <zen_darren@hotmail.com>
2018-12-19 01:40:35 +0800

    Minor Fix

--- NikeScraper.py
+++ NikeScraper.py
@@ -4,25 +4,27 @@
 # / /\  /| |   <  __/ / /__| (_| | |  __/ | | | (_| | (_| | |   
 # \_\ \/ |_|_|\_\___| \____/\__,_|_|\___|_| |_|\__,_|\__,_|_|   
 
-# CLI Based Nike Scraper Using GET Requests coded in Python
+# CLI Based Nike Scraper Using GET Requests & Python
 # Credits to danielbrzn for URL
 
 import requests
 import json
-from pyfiglet import figlet_format
 
 def NikeScraper():
     locales = ['SG', 'AU']
     for locale in locales:
-        url = 'https://www.nike.com/nikestore/html/services/launchCalendar?country={}&lang_locale=en_GB&sortOrder=asc'.format(locale)
-        r = requests.get(url)
-        itemList = json.loads(r.text)
         print('Nike {}'.format(locale))
-        for item in itemList['launchCalendarItems']:
-            print(item['month'], item['days'], '-', item['title'])
-
+        try:
+            url = 'https://www.nike.com/nikestore/html/services/launchCalendar?country={}&lang_locale=en_GB&sortOrder=asc'.format(locale)
+            r = requests.get(url)
+            itemList = json.loads(r.text)
+            for item in itemList['launchCalendarItems']:
+                print(item['month'], item['days'], '-', item['title'])
+        except:
+            print("Nothing listed on Calendar.")
+            
         print()
 
 if __name__ == '__main__':
-    print(figlet_format('Nike Calendar', font='doom'))
+    print('Nike Launch Calendar Loading...\n')
     NikeScraper()
